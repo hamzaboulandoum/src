634c2f97ffe5f0a25795e25a7822312346ce1431
Hamza Boulandoum <hamzaboulandoum@gmail.com>
2023-03-29 15:47:17 +0000

    updated_broadcaster_and_driver

---
+++ assistant/assistant/broadcaster.py
@@ -0,0 +1,113 @@
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import TransformStamped, Quaternion
+from nav_msgs.msg import Odometry
+from tf2_ros import TransformBroadcaster
+from math import sin, cos
+import serial
+import numpy as np
+
+#constants
+WHEEL_RADIUS = 0.05
+DISTANCE_FROM_CENTER = 0.18
+
+SYSTEM_MATRIX = 1/WHEEL_RADIUS*np.array([[1, 0, - DISTANCE_FROM_CENTER],[-1/2, -np.sqrt(3)/2, - DISTANCE_FROM_CENTER],[-1/2, np.sqrt(3)/2, - DISTANCE_FROM_CENTER]])
+
+class OdometryPublisher(Node):
+
+    def __init__(self):
+        super().__init__('odometry_publisher')
+        self.publisher_ = self.create_publisher(Odometry, 'odom', 50)
+        self.broadcaster_ = TransformBroadcaster(self)
+
+        self.x, self.y, self.th = 0.0, 0.0, 0.0
+        self.vx, self.vy, self.vth = 0.0, 0.0, 0.0
+
+        self.current_time = self.get_clock().now()
+        self.last_time = self.get_clock().now()
+
+        self.timer = self.create_timer(1.0, self.timer_callback)
+
+        self.declare_parameter('serial_port', value="/dev/ttyUSB0")
+        self.serial_port = self.get_parameter('serial_port').value
+
+        self.declare_parameter('baud_rate', value=9600)
+        self.baud_rate = self.get_parameter('baud_rate').value
+
+        self.serial = serial.Serial(self.serial_port, self.baud_rate)
+
+    def timer_callback(self):
+        encoder_data = self.serial.readline().decode().strip().split(', ')
+        while len(encoder_data) < 3 :
+            encoder_data = self.serial.readline().decode().strip().split(', ')
+
+        self.get_logger().info(str(encoder_data))
+
+        # Parse encoder data and calculate odometry values for a three-wheeled omnidirectional robot
+        u_1, u_2, u_3 = map(float, encoder_data)
+        U = np.array([u_1, u_2, u_3])
+        velocity = np.dot(np.linalg.inv(SYSTEM_MATRIX),U)
+        v_x = velocity[0]
+        v_y = velocity[1]
+        self.vx = v_x
+        self.vy = v_y
+        self.vth = 0.0
+        
+
+        self.current_time = self.get_clock().now()
+
+        dt = (self.current_time - self.last_time).nanoseconds / 1e9
+        delta_x = (self.vx * cos(self.th) - self.vy * sin(self.th)) * dt
+        delta_y = (self.vx * sin(self.th) + self.vy * cos(self.th)) * dt
+        delta_th = self.vth * dt
+
+        self.x += delta_x
+        self.y += delta_y
+        self.th += delta_th
+
+        odom_quat = Quaternion()
+        odom_quat.z = sin(self.th/2.0)
+        odom_quat.w = cos(self.th/2.0)
+
+        odom_trans = TransformStamped()
+        odom_trans.header.stamp = self.current_time.to_msg()
+        odom_trans.header.frame_id = 'odom'
+        odom_trans.child_frame_id = 'base_link'
+
+        odom_trans.transform.translation.x = self.x
+        odom_trans.transform.translation.y = self.y
+        odom_trans.transform.translation.z = 0.0
+        odom_trans.transform.rotation = odom_quat
+
+        self.broadcaster_.sendTransform(odom_trans)
+
+        odom = Odometry()
+        odom.header.stamp = self.current_time.to_msg()
+        odom.header.frame_id = 'odom'
+
+        odom.pose.pose.position.x = self.x
+        odom.pose.pose.position.y = self.y
+        odom.pose.pose.position.z = 0.0
+        odom.pose.pose.orientation = odom_quat
+
+        odom.child_frame_id = 'base_link'
+        odom.twist.twist.linear.x = self.vx
+        odom.twist.twist.linear.y = self.vy
+        odom.twist.twist.angular.z = self.vth
+
+        self.publisher_.publish(odom)
+
+        self.last_time = self.current_time
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    odom_publisher = OdometryPublisher()
+
+    rclpy.spin(odom_publisher)
+    odom_publisher.serial.close()
+    odom_publisher.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
--- assistant/assistant/driver.py
+++ assistant/assistant/driver.py
@@ -1,33 +1,40 @@
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
-from nav_msgs.msg import Odometry
-from std_msgs.msg import Header
 import serial
 import numpy as np
 import math
-import time
+
 
 #constants
 WHEEL_RADIUS = 0.05
 DISTANCE_FROM_CENTER = 0.18
+
 SYSTEM_MATRIX = 1/WHEEL_RADIUS*np.array([[1, 0, - DISTANCE_FROM_CENTER],[-1/2, -np.sqrt(3)/2, - DISTANCE_FROM_CENTER],[-1/2, np.sqrt(3)/2, - DISTANCE_FROM_CENTER]])
 
 class Driver(Node):
     def __init__(self):
         super().__init__('velocity_to_angle_node')
-        self.subscription = self.create_subscription(Twist, 'cmd_vel', self.callback, 10)
-        self.serial = serial.Serial('/dev/serial/by-path/pci-0000:00:14.0-usb-0:3:1.0', 9600) # change it for raspberry device when available
-        self.publisher = self.create_publisher(Odometry, 'odom', 10)
-        self.prev_x = 0.0
-        self.prev_y = 0.0
-        self.prev_t = time.time()
+        self.subscription = self.create_subscription(
+            Twist,
+            'cmd_vel',
+            self.callback,
+            10)
+        
+        self.declare_parameter('serial_port', value="/dev/ttyUSB0")
+        self.serial_port = self.get_parameter('serial_port').value
+
+        self.declare_parameter('baud_rate', value=9600)
+        self.baud_rate = self.get_parameter('baud_rate').value
+
+        self.serial = serial.Serial(self.serial_port, self.baud_rate)
+
+        
 
     def callback(self, msg):
         # Convert velocity commands to wheel speeds for a three-wheeled omnidirectional robot
         linear_velocity_x = msg.linear.x
         linear_velocity_y = msg.linear.y
-
         if linear_velocity_x == 0 and linear_velocity_y == 0:
             vx = 0
             vy = 0
@@ -38,56 +45,16 @@ class Driver(Node):
         data = str(vx) + " " + str(vy)        
         self.serial.write(data.encode()) # sending data to arduino 
 
-        # encoders
-        #a = len(encoder_data)
-        encoder_data = self.serial.readline().decode().strip().split(', ')
-        while len(encoder_data) < 3 :
-            encoder_data = self.serial.readline().decode().strip().split(', ')
 
-        self.get_logger().info(str(encoder_data))
 
-        # Parse encoder data and calculate odometry values for a three-wheeled omnidirectional robot
-        u_1, u_2, u_3 = map(float, encoder_data)
-        U = np.array([u_1, u_2, u_3])
-        velocity = np.dot(np.linalg.inv(SYSTEM_MATRIX),U)
-        v_x = velocity[0]
-        v_y = velocity[1]
 
-        
-        t = time.time()
-
-        x = self.prev_x + (t-self.prev_t)*v_x
-        y = self.prev_y + (t-self.prev_t)*v_y
-
-        self.prev_x = x
-        self.prev_y = y
-        self.prev_t = t
-        
-        odom_msg = Odometry()
-        odom_msg.header.frame_id = 'odom'
-        odom_msg.child_frame_id = 'base_link'
-        odom_msg.pose.pose.position.x = x
-        odom_msg.pose.pose.position.y = y
-        odom_msg.pose.pose.position.z = 0.0
-
-        odom_msg.twist.twist.linear.x = v_x
-        odom_msg.twist.twist.linear.y = v_y
-      
-        self.publisher.publish(odom_msg)
-        
 def main(args=None):
     rclpy.init(args=args)
     node = Driver()
     rclpy.spin(node)
+    node.serial.close()
     node.destroy_node()
     rclpy.shutdown()
 
-
 if __name__ == '__main__':
-
     main()
-
-
-
-
-    
